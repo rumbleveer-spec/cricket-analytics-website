44d503a67f44baa524196613aa0c817bba4da490
rumbleveer-spec <rumbleveer@gmail.com>
2025-12-01 22:03:05 +0300

    Add static/js/charts.js

---
+++ static/js/charts.js
@@ -0,0 +1,7 @@
+// Cricket Analytics Charts
+
+console.log('Cricket Analytics charts loaded');
+
+// Chart.js default configuration
+Chart.defaults.responsive = true;
+Chart.defaults.maintainAspectRatio = false;
